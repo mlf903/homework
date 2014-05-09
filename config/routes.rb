ddf657931ede1ab337e7add31887c465e7ae3be0
vagrant <vagrant@kellogg-dev-box>
2014-05-09 20:29:26 +0000

    user story 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Homework::Application.routes.draw do
   get 'dice/roll'
 
   get "/books" => 'books#list'
+
+  get "/books/:id" => 'books#show'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
